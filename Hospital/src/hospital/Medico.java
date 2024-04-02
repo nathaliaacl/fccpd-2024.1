bc69a9ca5a0e295544c5bb93a3a01cc64e73d6e7
mariblb1 <mblb@cesar.school>
2024-04-01 21:05:57 -0300

    versão nova de cliente e servidor

--- Hospital/src/hospital/Medico.java
+++ Hospital/src/hospital/Medico.java
@@ -1,113 +1,140 @@
 package hospital;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
 import java.net.NetworkInterface;
-import java.net.InetAddress;
-import java.net.DatagramPacket;
-import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class Medico {
-	public static void main(String[] args) throws IOException{
-		Scanner sc = new Scanner(System.in); 
-		byte[] envio = new byte[1024];
-		
-		MulticastSocket socket = new MulticastSocket(4321);
-		
-		InetAddress ia1 = InetAddress.getByName("230.0.0.1");
-        InetSocketAddress avisosGerais = new InetSocketAddress(ia1, 4321);
-        NetworkInterface ni1 = NetworkInterface.getByInetAddress(ia1);
-        
+    private static final int PORT = 4446;
+    private static List<String> topicosDisponiveis = Arrays.asList(
+        "230.0.0.1", // Tópico 1
+        "230.0.0.2", // Tópico 2
+        "230.0.0.3" // Tópico 3
+    );
+    private MulticastSocket socket;
+    private List<String> gruposSelecionados = new ArrayList<>();
+    private InetAddress group;
+    private boolean running = true;
+    private int escolha;
+    private String nomeRemetente;
+    private Boolean loop = true;
 
-		InetAddress ia2 = InetAddress.getByName("230.0.0.2");
-        InetSocketAddress avisosEmergencia = new InetSocketAddress(ia2, 4321);
-        NetworkInterface ni2 = NetworkInterface.getByInetAddress(ia2);
-        
+    Scanner scanner = new Scanner(System.in);
 
-        InetAddress ia3 = InetAddress.getByName("230.0.0.3");
-        InetSocketAddress grupoChat = new InetSocketAddress(ia3, 4321);
-        NetworkInterface ni3 = NetworkInterface.getByInetAddress(ia3);
-        
-        Runnable receberGerais = new ReceberMensagensCliente("230.0.0.1", 4321, "Avisos Gerais");
-        Thread threadRecebeGerais = new Thread(receberGerais);
-        threadRecebeGerais.start();
-        
-		System.out.println("Digite seu nome: ");
-		String nome = sc.nextLine();
-		
-		enviarHeader(socket, nome, ia1); 
-	
-		System.out.println("Você foi conectado ao grupo de avisos gerais. Opções de outros grupo para se juntar: \r\n"
-				+ "1- Grupo de avisos de emergências\r\n"
-				+ "2- Grupo de Chat para os médicos co hospital\r\n"
-				+ "3- Entrar para os dois grupos\r\n"
-				+ "4- Não entrar para mais nenhum grupo\r\n"); 		
-		
-		String topico = sc.nextLine();
-		
-		if(topico.equals("1")) {
-			
-	        Runnable receberEmergencia = new ReceberMensagensCliente("230.0.0.2", 4321, "Emergências");
-	        Thread threadRecebeEmergencia = new Thread(receberEmergencia);
-	        threadRecebeEmergencia.start();	        
+    public Medico() throws IOException {
+        this.socket = new MulticastSocket(PORT);
+    }
+    
+    public void entrada() throws IOException{
+    	addNome();
+    	escolherTopico(); 
+    }
 
-			enviarHeader(socket, nome, ia2);
-			
-		}else if(topico.equals("2")){
-
-	        Runnable receberChat = new ReceberMensagensCliente("230.0.0.3", 4321, "Chat");
-	        Thread threadRecebeChat = new Thread(receberChat);
-	        threadRecebeChat.start();
-            
-			enviarHeader(socket, nome, ia3); 
-	        
-	        while(true) {
-	            System.out.println("Digite sua mensagem para enviar ao grupo de chat:");
-	            String mensagem = nome +": " +sc.nextLine();
-		
-				envio = mensagem.getBytes(); 
-				
-				DatagramPacket pacote = new DatagramPacket(envio, envio.length, ia3, 4321);
-				socket.send(pacote);
+	public void escolherTopico() throws IOException {	
+        
+		while(loop) {
+	        System.out.println("Escolha um tópico (ou digite 0 para sair):");
+	        for (int i = 0; i < topicosDisponiveis.size(); i++) {
+	            System.out.println((i + 1) + ". " + topicosDisponiveis.get(i));
 	        }
-			
-		}else if(topico.equals("3")) {
-			
-			Runnable receberEmergencia = new ReceberMensagensCliente("230.0.0.2", 4321, "Emergências");
-	        Thread threadRecebeEmergencia = new Thread(receberEmergencia);
-	         
-	        Runnable receberChat = new ReceberMensagensCliente("230.0.0.3", 4321, "Chat");
-	        Thread threadRecebeChat = new Thread(receberChat);
 	        
-	        threadRecebeChat.start();
-	        threadRecebeEmergencia.start(); 
-
-			enviarHeader(socket, nome, ia2);
-			enviarHeader(socket, nome, ia3);    
+	        escolha = scanner.nextInt();
+	        scanner.nextLine();
 	        
-	        while(true) {
-	            System.out.println("Digite sua mensagem para enviar ao grupo de chat:");
-	            String mensagem = nome +": " +sc.nextLine();
-		
-				envio = mensagem.getBytes(); 
-				
-				DatagramPacket pacote = new DatagramPacket(envio, envio.length, ia3, 4321);
-				socket.send(pacote);
+	        /*if (escolha == 0) {
+	            System.out.println("Encerrando aplicação.");
+	            this.running = false;
+	            return;
+	        }*/
+	        
+	        if (escolha > 0 && escolha <= topicosDisponiveis.size()) {
+	            String enderecoTopico = topicosDisponiveis.get(escolha - 1);
+	            gruposSelecionados.add(enderecoTopico);
+	        } else {
+	            System.out.println("Escolha inválida. Por favor, escolha um número de tópico válido.");
+	        }
+	        
+	        System.out.println("Deseja se juntar a outro grupo?"
+	        		+ "1- Sim"
+	        		+ "2- Não");
+	        String continuar = scanner.nextLine();
+	        
+	        if(continuar.equals("2")){
+	        	loop = false; 
 	        }
 		}
-	}
-	
-	public static void enviarHeader(MulticastSocket socket, String nome, InetAddress ia) throws IOException {
-		byte[] envio = new byte[1024];
-		System.out.println("entro");
-	
-		String header = "entrada " + nome;
-		
-		envio = header.getBytes();
-		
-		DatagramPacket pacoteHeader = new DatagramPacket(envio, envio.length, ia, 4321);
-		
-		socket.send(pacoteHeader);
-	}
+        for(String grupo : gruposSelecionados) {
+            InetAddress ia = InetAddress.getByName(grupo);
+            InetSocketAddress isa = new InetSocketAddress(ia, PORT);
+            NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
+            
+            this.group = InetAddress.getByName(grupo);
+            this.socket.joinGroup(isa, ni);
+            
+            if(grupo.equals("230.0.0.3")) {
+                iniciarRecebimentoMensagens();
+                enviarMensagens(scanner);            
+            }else {       	
+                iniciarRecebimentoMensagens();       	
+            }
+        }		        
+        
+    }
+
+    private void iniciarRecebimentoMensagens() {
+        new Thread(() -> {
+            while (running) {
+                byte[] buffer = new byte[1000];
+                DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
+                try {
+                    socket.receive(messageIn);
+                    String received = new String(messageIn.getData(), 0, messageIn.getLength());
+                    System.out.println("Received: " + received);
+                } catch (IOException e) {
+                    System.out.println("Erro ao receber mensagem: " + e.getMessage());
+                }
+            }
+        }).start();
+    }
+
+    private void enviarMensagens(Scanner scanner) throws IOException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("[dd/MM/yyyy - HH:mm]");
+        System.out.println("Você pode enviar mensagens agora (digite '/sair' para sair do tópico):");
+        while (running) {
+            String msg = scanner.nextLine();
+            /*if ("/sair".equals(msg.toLowerCase())) {
+                sairTopico();
+                break;
+            }*/
+            String mensagemFormatada = dateFormat.format(new Date()) + " " + nomeRemetente + " : " + msg;
+            byte[] buffer = mensagemFormatada.getBytes();
+            DatagramPacket messageOut = new DatagramPacket(buffer, buffer.length, group, PORT);
+            socket.send(messageOut);
+        }
+    }
+    
+    private void addNome () {
+        System.out.println("Informe seu nome (ou identificador):");
+        nomeRemetente = scanner.nextLine();
+    }
+
+    @SuppressWarnings("deprecation")
+	private void sairTopico(InetSocketAddress isa, NetworkInterface ni) throws IOException {
+        socket.leaveGroup(isa, ni);;
+        System.out.println("Você saiu do tópico.");
+        escolherTopico(); // Dá a opção de escolher outro tópico ou sair
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Medico().entrada();
+    }
 }
--- Hospital/src/hospital/Recepcao.java
+++ Hospital/src/hospital/Recepcao.java
@@ -1,84 +1,115 @@
 package hospital;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
 import java.net.NetworkInterface;
-import java.net.InetAddress;
-import java.net.DatagramPacket;
-import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class Recepcao {
-	public static void main(String[] args) throws IOException{
-		byte[] envio = new byte[1024];
-		String topico;
-		String conteudo; 
-		String mensagem;
-		
-		Scanner sc = new Scanner(System.in); 
-		
-		MulticastSocket socket = new MulticastSocket(4321);
-		
-		InetAddress ia1 = InetAddress.getByName("230.0.0.1");
-        InetSocketAddress avisosGerais = new InetSocketAddress(ia1, 4321);
-        NetworkInterface ni1 = NetworkInterface.getByInetAddress(ia1);
-        
-        InetAddress ia2 = InetAddress.getByName("230.0.0.2");
-        InetSocketAddress avisosEmergencia = new InetSocketAddress(ia2, 4321);
-        NetworkInterface ni2 = NetworkInterface.getByInetAddress(ia2);
-        
-        InetAddress ia3 = InetAddress.getByName("230.0.0.3");
-        InetSocketAddress grupoChat = new InetSocketAddress(ia3, 4321);
-        NetworkInterface ni3 = NetworkInterface.getByInetAddress(ia3);
-        
-        socket.joinGroup(avisosGerais, ni1);
-        socket.joinGroup(avisosEmergencia, ni2);
-        socket.joinGroup(grupoChat, ni3);
-        
-        Runnable receberGerais = new ReceberMensagensServidor("230.0.0.1", 4321, "Avisos Gerais", ia1); 
-        Thread threadReceberGerais = new Thread(receberGerais);
-        threadReceberGerais.start();
-        
-        Runnable receberEmergencia = new ReceberMensagensServidor("230.0.0.2", 4321, "Avisos de emergência", ia2); 
-        Thread threadReceberEmergencia = new Thread(receberEmergencia);
-        threadReceberEmergencia.start();
-        
-        Runnable receberChat = new ReceberMensagensServidor("230.0.0.3", 4321, "Chat", ia3); 
-        Thread threadReceberChat = new Thread(receberChat);
-        threadReceberChat.start();
-        
-		
-        while(true) {
-        	System.out.print("[Servidor] Selecione para que grupo deseja enviar mensagem:\r\n"
-        			+ "1- Enviar mensagem para o grupo de avisos gerais\r\n"
-        			+ "2- Enviar mensagem para o grupo de aviso de emergência\r\n"
-        			+ "3- Enviar mensagem para o grupo chat.\r\n");
-        	topico = sc.nextLine();
-        	System.out.print("[Servidor] Digite o conteúdo da mensagem:");
-        	conteudo = sc.nextLine();
-        	System.out.println(topico);   
-        	
-        	mensagem = "Recepção: " + conteudo; 
+    private static final int PORT = 4446;
+    private static List<String> topicosDisponiveis = Arrays.asList(
+        "230.0.0.1", // Tópico 1
+        "230.0.0.2", // Tópico 2
+        "230.0.0.3" // Tópico 3
+    );
+    private MulticastSocket socket;
+    private List<String> gruposSelecionados = new ArrayList<>();
+    private InetAddress group;
+    private boolean running = true;
+    private String nomeRemetente;
+    private List<InetAddress> grupos = new ArrayList<>();
 
-    		//formatar a mensgem direitinho
-        	if(topico.equals("1")) {
-        		envio = mensagem.getBytes();
-        		
-        		DatagramPacket pacote = new DatagramPacket(envio, envio.length, ia1, 4321);
-        		socket.send(pacote);
-        		
-        	}else if(topico.equals("2")) {
-        		envio = mensagem.getBytes();
-        		
-        		DatagramPacket pacote = new DatagramPacket(envio, envio.length, ia2, 4321);
-        		socket.send(pacote);
-        	}else if(topico.equals("3")) {
-        		envio = mensagem.getBytes();
-        		
-        		DatagramPacket pacote = new DatagramPacket(envio, envio.length, ia3, 4321);
-        		socket.send(pacote);
-        	}
+    Scanner scanner = new Scanner(System.in);
+
+    public Recepcao() throws IOException {
+        this.socket = new MulticastSocket(PORT);
+    }
+    
+    public void entrada() throws IOException{
+    	addNome();
+    	entrarTopicos(); 
+    }
+
+	public void entrarTopicos() throws IOException {	
+        for(String grupo : topicosDisponiveis) {
+            InetAddress ia = InetAddress.getByName(grupo);
+            InetSocketAddress isa = new InetSocketAddress(ia, PORT);
+            NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
+            
+            this.group = InetAddress.getByName(grupo);
+            this.socket.joinGroup(isa, ni);
+            iniciarRecebimentoMensagens();
         }
-		
-	}
+        enviarMensagens(scanner);
+        
+    }
+
+    private void iniciarRecebimentoMensagens() {
+        new Thread(() -> {
+            while (running) {
+                byte[] buffer = new byte[1000];
+                DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
+                try {
+                    socket.receive(messageIn);
+                    String received = new String(messageIn.getData(), 0, messageIn.getLength());
+                    System.out.println("Received: " + received);
+                } catch (IOException e) {
+                    System.out.println("Erro ao receber mensagem: " + e.getMessage());
+                }
+            }
+        }).start();
+    }
+
+    private void enviarMensagens(Scanner scanner) throws IOException {
+    	 SimpleDateFormat dateFormat = new SimpleDateFormat("[dd/MM/yyyy - HH:mm]");
+         while (true) {
+             System.out.println("Escolha o número do grupo para enviar a mensagem:");
+             for (int i = 0; i < topicosDisponiveis.size(); i++) {
+                 System.out.println((i + 1) + ". " + topicosDisponiveis.get(i));
+             }
+             int escolha = scanner.nextInt();
+             scanner.nextLine(); 
+
+             if (escolha < 1 || escolha > topicosDisponiveis.size()) {
+                 System.out.println("Escolha inválida. Tente novamente.");
+                 continue;
+             }
+             String enderecoTopico = topicosDisponiveis.get(escolha - 1);
+             
+             InetAddress ia = InetAddress.getByName(enderecoTopico);
+
+             System.out.println("Digite a mensagem:");
+             String mensagem = scanner.nextLine();
+
+             String mensagemFormatada = dateFormat.format(new Date()) + " " + nomeRemetente + " : " + mensagem;
+             byte[] buffer = mensagemFormatada.getBytes();
+
+             DatagramPacket messageOut = new DatagramPacket(buffer, buffer.length, ia, PORT);
+             socket.send(messageOut);
+         }
+    }
+    
+    private void addNome () {
+        System.out.println("Informe seu nome (ou identificador):");
+        nomeRemetente = scanner.nextLine();
+    }
+
+    /*@SuppressWarnings("deprecation")
+	private void sairTopico(InetSocketAddress isa, NetworkInterface ni) throws IOException {
+        socket.leaveGroup(isa, ni);;
+        System.out.println("Você saiu do tópico.");
+        entrarTopicos(); // Dá a opção de escolher outro tópico ou sair
+    }*/
+
+    public static void main(String[] args) throws IOException {
+        new Recepcao().entrada();
+    }
 }
